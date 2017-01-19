a5917f9b7003e18de2adbb96a865caa8bd97f1f6
Clement Wekesa <clemwek@gmail.com>
2017-01-19 15:01:12 +0300

    final fixing

--- car_class.py
+++ car_class.py
@@ -10,34 +10,30 @@ class Car(object):
         self.car_type = ''
 
         if self.name == 'Koenigsegg' or self.name == 'Porshe':
-        	self.num_of_doors = 2
+            self.num_of_doors = 2
         else: 
-        	self.num_of_doors = 4
+            self.num_of_doors = 4
 
         if self.vehicle_type=='trailer':
-        	self.num_of_wheels = 8
+            self.num_of_wheels = 8
         else:
-        	self.num_of_wheels = 4
+            self.num_of_wheels = 4
 
-        try:
-        	def saloon(self):
-				if self.vehicle_type == 'saloon':
-					return True
-				else:
-					return False
-					
-			def drive(self, moving_speed):
-				if moving_speed == 3:
-					Car.speed = 1000
-				elif moving_speed == 7:
-					Car.speed = 77
-				return self
+    try:
+        def saloon(self):
+            if self.vehicle_type == 'saloon':
+                return True
+            else:
+                return False
 
-		except Exception:
-			return 'error'
-
-    def drive():
-        pass
+        def drive(self, moving_speed):
+            if moving_speed == 3:
+                Car.speed = 1000
+            elif moving_speed == 7:
+                Car.speed = 77
+            return self
+    except Exception:
+        return 'Error'
 
 
 
